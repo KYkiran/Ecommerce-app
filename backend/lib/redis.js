c2b003458651e8f5a0d0c03bc48ff1eabb8ab005
Yuva Kiran <kiran.yuva4@gmail.com>
2025-06-07 16:36:13 +0530

    Analytics Route

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.UPSTASH_REDIS_URL);
-
 if (!process.env.UPSTASH_REDIS_URL) {
   throw new Error("UPSTASH_REDIS_URL is not defined in environment variables");
 }
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import connectMongoDB from "./lib/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -21,7 +22,8 @@ app.use('/api/auth',authRoutes);
 app.use('/api/products',productRoutes);
 app.use('/api/cart',cartRoutes);
 app.use('/api/coupons',couponRoutes);
-app.use('/api/payment',paymentRoutes)
+app.use('/api/payment',paymentRoutes);
+app.use('/api/analytics',analyticsRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is Running on port:${PORT}`);

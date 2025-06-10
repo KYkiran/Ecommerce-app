b14712fe5e3d7e26f23abd125085f3d63dfb3582
Yuva Kiran <kiran.yuva4@gmail.com>
2025-06-10 10:59:42 +0530

    Undo changes

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ app.use('/api/payments',paymentRoutes);
 app.use('/api/analytics',analyticsRoutes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "frontend/")));
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
 
